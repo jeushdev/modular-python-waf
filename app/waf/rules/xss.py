5a534b52ea009fbcf27b9055b32e511150eecd8d
jeushdev <jeushsamuelb@gmail.com>
2026-06-06 13:46:49 +0800

    fix: edit update xss_pattern to filter more keywords

--- app/waf/rules/xss.py
+++ app/waf/rules/xss.py
@@ -1,6 +1,5 @@
 from app.waf.rules.base import BaseRule
 from app.waf.models.request_context import RequestContext
-from abc import ABC, abstractmethod
 import re
 
 class XSSRule(BaseRule):
@@ -8,7 +7,7 @@ class XSSRule(BaseRule):
         super().__init__("Cross-Site Scripting Engine", "CRITICAL")
     
     def evaluate(self, context: RequestContext) -> bool:
-        xss_pattern = r"<script.*?>|<\/script>|javascript:|onerror=|onload=|html|<body>|<iframe"
+        xss_pattern = r"<script.*?>|<\/script>|javascript:|onerror=|onload=|html|<body>|<iframe|alert\(|confirm\(|prompt\("
         for value in context.inputs.values():
             if re.search(xss_pattern, value, re.IGNORECASE):
                 return True
